58fca878bd8961ae4dd7f6980d2f8aafd6eff8a6
ryan kilbane <ryankilbane92@hotmail.com>
2023-06-20 18:21:14 +0100

    feat: add heartbeat and ability to read from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,38 @@
 mod connection;
+use tokio;
+use std::io;
+use std::sync::Arc;
+use message::{client_server::NormalMessage, message::{MessageContainer, MessageTypes}, client_server_trait::ClientServer};
 
 #[tokio::main(flavor="current_thread")]
-async fn main() {
+async fn main() -> io::Result<()>{
     let connection = connection::ClientConnection::new("http://en.wikipedia.org".to_string());
     let connection = match connection.connect().await{
-        Ok(connected) => connected,
+        Ok(connected) => Arc::new(connected),
         Err(e) => {
             eprintln!("{}", e);
             panic!()
         }
-    };
+    }; 
+
+    let heartbeat_connection = connection.clone();
+    tokio::spawn(async move {
+        loop{
+            heartbeat_connection.send_heartbeat().await;
+            println!("Sent heartbeat");
+            tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+        }
+    });
+
+    loop{
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer)?;
+        let container = MessageContainer::new(buffer, 
+            MessageTypes::Normal, None);
+        
+        let message = NormalMessage::new(container);
+        println!("{:?}", message);
+        connection.send(message).await;
+
+    }
 }
